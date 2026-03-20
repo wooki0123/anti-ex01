c5460f646e19b5c17a97f6fe58ad4a4c9bb45117
wooki0123 <wookimini22@gmail.com>
2026-03-20 12:50:24 +0900

    10강 진행

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,12 +1,23 @@
 'use client'
 
 import { useParams } from 'next/navigation'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function PostDetail() {
   const { id } = useParams()
+  const [post, setPost] = useState({})
 
-  useState({})
+  useEffect(() => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then((res) => res.json())
+      .then((res) => setPost(res))
+  }, [])
 
-  return <>{id} 번 게시글 상세</>
+  return (
+    <>
+      <div>{post.id} 번 게시글 상세</div>
+      <div>{post.title}</div>
+      <div>{post.body}</div>
+    </>
+  )
 }
